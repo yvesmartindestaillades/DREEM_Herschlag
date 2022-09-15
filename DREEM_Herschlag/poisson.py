616ce9134a3391e889b7dd2a1c3ddae80a3c3f17
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2022-09-14 17:00:24 -0700

    changed poisson built-in method for qi2

--- DREEM_Herschlag/poisson.py
+++ DREEM_Herschlag/poisson.py
@@ -1,12 +1,13 @@
 import scipy.stats
 import numpy as np
 
-def compute_conf_interval(cov_bases, mut_bases):
+def compute_conf_interval(cov_bases, mut_bases, alpha = 0.05):
     assert len(cov_bases)==len(mut_bases), "cov_bases and mut_bases must be of same length"
     ci = {}
     i, ci['min'], ci['max'], ci['low'], ci['high'] = 0, np.zeros(len(cov_bases)), np.zeros(len(cov_bases)), np.zeros(len(cov_bases)), np.zeros(len(cov_bases))
     for cov, mut in zip(cov_bases, mut_bases):
-        ci['min'][i], ci['max'][i] = tuple(np.array(scipy.stats.poisson.interval(0.95, mut))/cov)
+       # ci['min'][i], ci['max'][i] = tuple(np.array(scipy.stats.poisson.interval(0.95, mut))/cov)
+        ci['min'][i], ci['max'][i] = 0.5*scipy.stats.chi2.ppf(alpha/2, df=2*mut)/cov, 0.5*scipy.stats.chi2.ppf(1-alpha/2, df=2*(mut+1))/cov
         ci['low'][i], ci['high'][i] = mut/cov-ci['min'][i], ci['max'][i]-mut/cov
         i = i+1
     return ci
